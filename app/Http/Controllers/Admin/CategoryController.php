ee4d4a9b2de7b08539929cf29eeef9067e91f601
GiulioSpugnini <giuliospugnini@yahoo.com>
2022-03-29 21:31:02 +0200

    implemented CategoryController show

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -58,7 +58,7 @@ class CategoryController extends Controller
      */
     public function show(Category $category)
     {
-        //
+        return view('admin.categories.show', compact('category'));
     }
 
     /**
